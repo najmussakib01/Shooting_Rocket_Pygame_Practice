408d0af02e507defb28527af6863baaad21dc808
najmussakib01 <n.skib.k@gmail.com>
2025-08-23 13:22:09 +0600

    Create a screen

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
+from tensorflow.python.distribute.cluster_resolver.tpu.tpu_cluster_resolver import is_running_in_gce
 
 pygame.init()
 
 screen_size = pygame.display.set_mode((800, 800))
+is_running = True
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
 
 
