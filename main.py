d0a9110a88ba206da15435e71795f945f145d4c5
najmussakib01 <n.skib.k@gmail.com>
2025-08-23 14:45:54 +0600

    Add player

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
-from tensorflow.python.distribute.cluster_resolver.tpu.tpu_cluster_resolver import is_running_in_gce
 
 pygame.init()
-
+pygame.display.set_caption("Shooting Rocket")
+pygame.display.set_icon(pygame.image.load("rocket.png"))
 screen_size = pygame.display.set_mode((800, 800))
+
+player_image = pygame.image.load("rocket.png")
+playerX = 368
+playerY = 736
+
+def player_draw():
+    screen_size.blit(player_image, (playerX, playerY))
+
 is_running = True
 while True:
+    screen_size.fill((60, 70, 110))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
-
+    player_draw()
+    pygame.display.update()
