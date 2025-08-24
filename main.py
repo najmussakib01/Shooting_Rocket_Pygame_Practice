7727cc2a94afc3ef38560c7004c8e765b55e773a
najmussakib01 <n.skib.k@gmail.com>
2025-08-24 15:17:34 +0600

    add some score function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+import math
 
 pygame.init()
 pygame.display.set_caption("Shooting Rocket")
@@ -15,16 +16,18 @@ playerX_change = 0
 
 enemy_image = pygame.image.load("rocket.png")
 enemyX = random.randint(0, 736)
-enemyY = random.randint(50, 150)
-enemyX_change = 1
+enemyY = random.randint(50, 200)
+enemyX_change = 0.5
 enemyY_change = 50
 
 bullet_image = pygame.image.load("bullet.png")
 bulletX = 0
 bulletY = 500
 bulletX_change = 0
-bulletY_change = 1
+bulletY_change = 3
 visible_bullet = False
+
+score = 0
 
 def player_draw(x,y):
     screen_size.blit(player_image, (x, y))
@@ -36,6 +39,16 @@ def fire_bullet(x,y):
     global visible_bullet
     visible_bullet = True
     screen_size.blit(bullet_image, (x +16 , y + 10))
+
+
+def there_is_collision(enemyX, enemyY, bulletX, bulletY):
+    #distance = ((enemyX - bulletX)**2 + (enemyY - bulletY)**2)**0.5
+    distance = math.sqrt((math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
+
 
 is_running = True
 while True:
@@ -85,12 +98,18 @@ while True:
     if bulletY <= -64:
         bulletY = 500
         visible_bullet = False
-
-
     if visible_bullet:
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
+    collision = there_is_collision(enemyX, enemyY, bulletX, bulletY)
+    if collision:
+        bulletY = 500
+        visible_bullet = False
+        score += 1
+        print(score)
+        #enemyX = random.randint(0, 736)
+        #enemyY = random.randint(50, 200)
 
     player_draw(playerX, playerY)
     enemy_draw(enemyX, enemyY)
